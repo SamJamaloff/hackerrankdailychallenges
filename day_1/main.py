c6d92c43aebe63f20bcb7911ef3244c9429d476a
Sam Jamaloff <samjamaloff@gmail.com>
2020-12-22 18:42:50 +0900

    Day 1. Done!!!

--- day_1/main.py
+++ day_1/main.py
@@ -1,8 +1,15 @@
-# Read a full line of input from stdin and save it to our dynamically typed variable, input_string.
-input_string = input()
-
-# Print a string literal saying "Hello, World." to stdout.
-print('Hello, World.')
-
-# TODO: Write a line of code here that prints the contents of input_string to stdout.
-print(input_string)
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+# Read and save an integer, double, and String to your variables.
+integer_value = int(input())
+double_value = float(input()) # there is no double in python it is only float
+string_value = str(input())
+# Print the sum of both integer variables on a new line.
+print(i+integer_value)
+# Print the sum of the double variables on a new line.
+print(d+double_value)
+# Concatenate and print the String variables on a new line
+print(s+string_value)
+# The 's' variable above should be printed first.
