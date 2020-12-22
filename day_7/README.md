aba636f3e9ce275ccf63fd4f58a869d80d7b1220
Sam Jamaloff <samjamaloff@gmail.com>
2020-12-22 19:53:42 +0900

    Day 7. Done!!!

--- day_7/README.md
+++ day_7/README.md
@@ -1,1 +1,1 @@
-
+This is Day 7 of HackerRank 30 days challenge. The original problem is given in https://www.hackerrank.com/challenges/30-arrays/problem
--- day_7/main.py
+++ day_7/main.py
@@ -1,1 +1,11 @@
+if __name__ == '__main__':
+    n = int(input())
 
+    arr = list(map(int, input().rstrip().split()))
+    reversed_arr = [i for i in reversed(arr)]
+    print(reversed_arr)
+    
+    print_string = ""
+    for i in reversed_arr:
+        print_string +=(str(i)+" ")
+    print(print_string)
