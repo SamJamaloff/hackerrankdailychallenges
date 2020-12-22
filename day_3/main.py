fb1ac8a67d2525a6cd489c5bbd5a1964216b66d7
Sam Jamaloff <samjamaloff@gmail.com>
2020-12-22 18:53:12 +0900

    Day 3. Done!!!

--- day_3/main.py
+++ day_3/main.py
@@ -1,1 +1,11 @@
-
+#!/bin/python3
+if __name__ == '__main__':
+    N = int(input())
+    if N % 2 == 1: # in here we need to print "wierd"
+        print("Weird")
+    elif N % 2 == 0 and 2 <= N <= 5:
+        print("Not Weird")
+    elif N % 2 == 0 and 6 <= N <= 20:
+        print("Weird")
+    elif N % 2 == 0 and 20 < N:
+        print("Not Weird")
