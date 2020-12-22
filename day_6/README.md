4c043a30a54a68ea9f4142b9c779a1d8f7dbee3e
Sam Jamaloff <samjamaloff@gmail.com>
2020-12-22 19:41:22 +0900

    Day 7. Done!!!

--- day_6/README.md
+++ day_6/README.md
@@ -1,1 +1,2 @@
 
+This is Day 6 of HackerRank 30 days challenge. The original problem is given in https://www.hackerrank.com/challenges/30-review-loop/problem
--- day_6/main.py
+++ day_6/main.py
@@ -1,1 +1,13 @@
+t = int(input())
+for i in range(t):
+    s = input()
+    odd_order = ""
+    even_order = ""
+    for order in range(len(s)):
+        if (order + 1) % 2 == 1:
+            odd_order = odd_order+s[order]
 
+        elif (order + 1) % 2 == 0:
+            even_order = even_order+s[order]
+
+    print(odd_order+" "+even_order)
