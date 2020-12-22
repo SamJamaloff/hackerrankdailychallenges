30ef88fc244f9b00d5f9b76bc3cbcd84328bd507
Sam Jamaloff <samjamaloff@gmail.com>
2020-12-22 21:17:24 +0900

    Day 8. Done!!!

--- day_8/README.md
+++ day_8/README.md
@@ -1,1 +1,1 @@
-
+This is Day 8 of HackerRank 30 days challenge. The original problem is given in https://www.hackerrank.com/challenges/30-dictionaries-and-maps/problem
--- day_8/main.py
+++ day_8/main.py
@@ -1,1 +1,13 @@
+n = int(input())
+phoneBooks = {}
+for i in range(n):
+    i_val = input().split(" ")
+    phoneBooks[i_val[0]] = i_val[1] 
 
+
+for i in range(n):
+    check_string = input()
+    if check_string in phoneBooks:
+        print(check_string + "=" + phoneBooks[check_string])
+    else:
+        print("Not found")
