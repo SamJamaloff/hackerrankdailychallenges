1d03665ec83ae2c0b55845ae4dde5bb1df277c8c
Sam Jamaloff <samjamaloff@gmail.com>
2020-12-23 20:08:30 +0900

    Day 8. Modified.

--- day_8/main.py
+++ day_8/main.py
@@ -5,9 +5,12 @@ for i in range(n):
     phoneBooks[i_val[0]] = i_val[1] 
 
 
-for i in range(n):
-    check_string = input()
-    if check_string in phoneBooks:
-        print(check_string + "=" + phoneBooks[check_string])
-    else:
-        print("Not found")
+while True:
+    try:
+        check_string = input()
+        if check_string in phoneBooks:
+            print(check_string + "=" + phoneBooks[check_string])
+        else:
+            print("Not found")
+    except Exception as e:
+        pass
