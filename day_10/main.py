0c2e56ca113f5523b02efc66d33d4e18d16fab8e
Sam Jamaloff <samjamaloff@gmail.com>
2021-01-03 12:04:09 +0900

    Day 10. Done !!!

--- day_10/main.py
+++ day_10/main.py
@@ -1,1 +1,16 @@
+if __name__ == '__main__':
+    # n = int(input())
+    n = 125
+    cc_one = 0
+    mc_one = 0
+    while n > 0:
+        r = n % 2
+        if r == 1:
+            cc_one +=1
+            if cc_one > mc_one:
+                mc_one = cc_one
+        else: 
+            cc_one = 0
+        n = n // 2
 
+    print(mc_one)
