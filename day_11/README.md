3d5c51c62059f121eb40bd11a13b3cbdbed6ddf3
Sam Jamaloff <samjamaloff@gmail.com>
2021-01-04 20:32:06 +0900

    Day 11. Done!!!

--- day_11/README.md
+++ day_11/README.md
@@ -1,1 +1,2 @@
 
+This is Day 10 of HackerRank 30 days challenge. The original problem is given in https://www.hackerrank.com/challenges/30-2d-arrays/problem
--- day_11/main.py
+++ day_11/main.py
@@ -1,1 +1,28 @@
+if __name__ == '__main__':
 
+    def get_sum(matrix, row, col):
+        sum = 0
+        sum += matrix[row][col]
+        sum += matrix[row-1][col-1]
+        sum += matrix[row-1][col]
+        sum += matrix[row-1][col+1]
+        sum += matrix[row+1][col-1]
+        sum += matrix[row+1][col]
+        sum += matrix[row+1][col+1]
+        return sum
+
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    max_sum = -63
+    summs = []
+    for i in range(1, 5):
+        for j in range(1,5):
+            summs.append(get_sum(arr, i, j))
+             
+    print(max(summs))
+
+    
