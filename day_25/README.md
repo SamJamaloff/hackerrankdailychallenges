5c31587c96ba5da284785b7ae45aa2a2d515dd3e
Sam Jamaloff <samjamaloff@gmail.com>
2021-02-06 21:34:34 +0900

    Day 25. Done!!!

--- day_25/README.md
+++ day_25/README.md
@@ -1,1 +1,2 @@
 
+This is Day 25 of HackerRank 30 days challenge. The original problem is given in https://www.hackerrank.com/challenges/30-running-time-and-complexity/problem
--- day_25/main.py
+++ day_25/main.py
@@ -1,1 +1,21 @@
+import math
 
+def isPrime(number):
+	if number < 0:
+		return False
+	sqrt_number = math.sqrt(number)
+	if sqrt_number.is_integer():
+		return False
+	for i in range(2, int(sqrt_number)+1):
+		if n % i == 0:
+			return False
+	return True
+
+test_cases = int(input())
+for i in range(test_cases):
+	n = int(input())
+	if isPrime(n):
+		print("Prime")
+	else:
+		print("Not prime")
+	
